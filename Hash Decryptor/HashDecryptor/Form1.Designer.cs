ecbfe6d1ce51e6cb277254e47c36407d1086ddfb
iwilliams89 <57196729+iwilliams89@users.noreply.github.com>
2020-08-02 09:03:15 -0400

    Initial Upload of Hash Decryptor

---
+++ Hash Decryptor/HashDecryptor/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace HashDecryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CurHash = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AttemptCount = new System.Windows.Forms.Label();
+            this.ASCIIOutput = new System.Windows.Forms.Label();
+            this.HashOutput = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.HashRate = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hash To Decrypt";
+            // 
+            // CurHash
+            // 
+            this.CurHash.Location = new System.Drawing.Point(97, 23);
+            this.CurHash.Name = "CurHash";
+            this.CurHash.Size = new System.Drawing.Size(282, 20);
+            this.CurHash.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.AttemptCount);
+            this.groupBox1.Controls.Add(this.ASCIIOutput);
+            this.groupBox1.Controls.Add(this.HashOutput);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(420, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 137);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hash Results";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Attempt Count:";
+            // 
+            // AttemptCount
+            // 
+            this.AttemptCount.AutoSize = true;
+            this.AttemptCount.Location = new System.Drawing.Point(6, 106);
+            this.AttemptCount.Name = "AttemptCount";
+            this.AttemptCount.Size = new System.Drawing.Size(13, 13);
+            this.AttemptCount.TabIndex = 4;
+            this.AttemptCount.Text = "0";
+            // 
+            // ASCIIOutput
+            // 
+            this.ASCIIOutput.AutoSize = true;
+            this.ASCIIOutput.Location = new System.Drawing.Point(6, 39);
+            this.ASCIIOutput.Name = "ASCIIOutput";
+            this.ASCIIOutput.Size = new System.Drawing.Size(0, 13);
+            this.ASCIIOutput.TabIndex = 3;
+            // 
+            // HashOutput
+            // 
+            this.HashOutput.AutoSize = true;
+            this.HashOutput.Location = new System.Drawing.Point(6, 70);
+            this.HashOutput.Name = "HashOutput";
+            this.HashOutput.Size = new System.Drawing.Size(0, 13);
+            this.HashOutput.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Result:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ASCII Equivelant";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.CurHash);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(402, 137);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hash Properties";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(6, 57);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(390, 73);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Run";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 160);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "MD5 Hash Decryptor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CurHash;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label AttemptCount;
+        private System.Windows.Forms.Label ASCIIOutput;
+        private System.Windows.Forms.Label HashOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer HashRate;
+    }
+}
+
---
+++ Hash Decryptor/HashDecryptor/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HashDecryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static char[] fCharList =
+        {
+        '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j' ,'k','l','m','n','o','p',
+                    'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+                    'Q','R','S','T','U','V','X','Y','Z','~','!','@','#','$','%','^','&','*','(',')','[',']','{','}','.',',','/','?','\'','|','"',';',':','<','>','\\','=','-','+','`','_'
+        };
+        /*private static char[] fCharList =
+        {
+        '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j' ,'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+        };*/
+        private StringBuilder psb = new StringBuilder();
+        private ulong len;
+        private int _max;
+        private int _min;
+        public int max { get { return _max; } set { _max = value; } }
+        public int min { get { return _min; } set { _min = value; } }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(CurHash.Text))
+            {
+               Decrypt();
+            }
+        }
+
+        public void Decrypt()
+        {
+            var threat = new Thread(() =>
+            {
+                string password = fCharList[0].ToString();
+                for (int i = 0; i < 5000000; i++)
+                {
+                    this.ASCIIOutput.BeginInvoke((MethodInvoker)delegate () { this.ASCIIOutput.Text = password.ToString(); this.ASCIIOutput.Invalidate(); this.ASCIIOutput.Update(); ; });
+                    this.HashOutput.BeginInvoke((MethodInvoker)delegate () { this.HashOutput.Text = CalculateMD5Hash(password); this.HashOutput.Invalidate(); this.HashOutput.Update(); ; });
+                    this.AttemptCount.BeginInvoke((MethodInvoker)delegate () { this.AttemptCount.Text = i.ToString(); this.AttemptCount.Invalidate(); this.AttemptCount.Update(); ; });
+                    if (HashOutput.Text == CurHash.Text.ToUpper())
+                    {
+                        ASCIIOutput.ForeColor = Color.Green;
+                        break;
+                    }
+                    password = IncreasePassword(password);
+                }
+            });
+            threat.Start();
+        }
+
+        public static string IncreasePassword(String pass)
+        {
+            bool changed = false;
+            StringBuilder sb = new StringBuilder(pass);
+            for (int i = 0; i < pass.Length && !changed; i++)
+            {
+                int index = Array.IndexOf(fCharList, sb[i]);
+                if (index < fCharList.Length - 1)
+                {
+                    for (int j = i - 1; j >= 0 && sb[j] == fCharList[fCharList.Length - 1]; j--)
+                    {
+                        sb[j] = fCharList[0];
+                    }
+                    sb[i] = fCharList[index + 1];
+                    changed = true;
+                }
+            }
+            if (!changed)
+            {
+                return "".PadLeft(pass.Length + 1, fCharList[0]);
+            }
+            return sb.ToString();
+        }
+
+        public String CalculateMD5Hash(String Input)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(Input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
